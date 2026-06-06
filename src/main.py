f9265ab890a3695035e6fc6c81f9f5d967ac40a2
kern-51 <soonyoukern@gmail.com>
2026-06-06 16:17:44 +0800

    feat: main.py framework
    some code to give the file structure

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
 
+from preprocessing import clean_data
+from imputation import run_knn_imputer
+from model import train_model
+
+def pipeline():
+    df = data_ingest("raw_data.csv")
+    df = data_clean(df)
+    df = run_knn_imputer(df, k=7)
+    train_model(df)
+
+if __name__ == "__main__":
+    pipeline()
